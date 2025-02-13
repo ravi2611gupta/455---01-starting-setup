55c7e590091b5c24ce412ce53f228a27a3f505ce
ravi <ravi2611gupta@gmail.com>
2025-02-13 18:12:37 +0530

    465 videos done

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { selectCount, selectDoubleCount } from '../store/counter.selectors';
 
 @Component({
   selector: 'app-counter-output',
@@ -10,8 +11,11 @@ import { Observable } from 'rxjs';
 export class CounterOutputComponent {
   // counter = 0;
   count$: Observable<number>;
+  doubleCount$: Observable<number>;
 
   constructor(private store: Store<{ counter: number }>) {
-    this.count$ = store.select('counter');
+    // this.count$ = store.select('counter');
+    this.count$ = store.select(selectCount);
+    this.doubleCount$ = store.select(selectDoubleCount)
   }
 }
